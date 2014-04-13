fdd99e063f3ba04487dc92eefc3a041098f5c0d0
coderdiaz <javier.diaz.internaut@gmail.com>
2014-04-13 00:01:12 -0500

    Agregado Foundation v5.2.2 para uso frontend

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -3,6 +3,21 @@
 	<head>
 		<meta charset="UTF-8" />
 		<title>Apnote | Solucionando tus problemas, Mejorando tus procesos</title>
+		<!-- Styles for the app -->
+		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/foundation.min.css');?>" />
+		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/normalize.css');?>" />
 	</head>
-	<body></body>
+	<body>
+		<!-- Main body -->
+		
+		<!-- End Main body -->
+
+		<!-- Scripts for the app -->
+		<script type="text/javascript" src="<?=base_url('assets/js/vendor/jquery.js');?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/vendor/modernizr.js');?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/foundation.min.js');?>"></script>
+		<script type="text/javascript">
+			$(document).foundation();
+		</script>
+	</body>
 </html>
