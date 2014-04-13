8fbe1184b4c75b6b2ca8ca653faeff82519bd8a6
coderdiaz <javier.diaz.internaut@gmail.com>
2014-04-13 12:30:11 -0500

    Agregadas las columnas

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,7 +1,10 @@
 <?php $this->load->view('header'); ?>
 
 		<!-- Main body -->
-		
+		<div class="app-main-body">
+			<div class="large-2 columns"></div>
+			<div class="large-8 columns"></div>
+		</div>
 		<!-- End Main body -->
 
 <?php $this->load->view('footer'); ?>
