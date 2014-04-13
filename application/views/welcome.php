c4a608381a9c3cdba71fbb773df3990db450005c
coderdiaz <javier.diaz.internaut@gmail.com>
2014-04-13 00:19:35 -0500

    Modificación del frontend en welcome.php y agregado app.css

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html>
+<!-- [if IE 9]><html class="lt-ie10" lang="ES_es" ><![endif] -->
+<html class="no-js" lang="ES_es">
 	<head>
 		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Apnote | Solucionando tus problemas, Mejorando tus procesos</title>
 		<!-- Styles for the app -->
 		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/foundation.min.css');?>" />
 		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/normalize.css');?>" />
+		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/app.css');?>" />
+		<!-- Scripts -->
+		<script type="text/javascript" src="<?=base_url('assets/js/vendor/modernizr.js');?>"></script>
 	</head>
 	<body>
 		<!-- Main body -->
@@ -14,9 +19,8 @@
 
 		<!-- Scripts for the app -->
 		<script type="text/javascript" src="<?=base_url('assets/js/vendor/jquery.js');?>"></script>
-		<script type="text/javascript" src="<?=base_url('assets/js/vendor/modernizr.js');?>"></script>
 		<script type="text/javascript" src="<?=base_url('assets/js/foundation.min.js');?>"></script>
-		<script type="text/javascript">
+		<script>
 			$(document).foundation();
 		</script>
 	</body>
