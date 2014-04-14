a149bca3d7213c4ca75ae2f15f6db4691c7e332d
coderdiaz <javier.diaz.internaut@gmail.com>
2014-04-14 17:00:57 -0500

    Información de Servicios y frase

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,7 @@
 		<!-- Scripts for the app -->
 		<script type="text/javascript" src="<?=base_url('assets/js/vendor/jquery.js');?>"></script>
 		<script type="text/javascript" src="<?=base_url('assets/js/foundation.min.js');?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/foundation/foudnation.equalizer.js');?>"></script>
 		<script>$(document).foundation();</script>
 	</body>
 </html>
--- application/views/welcome.php
+++ application/views/welcome.php
@@ -50,6 +50,41 @@
 					<div class="large-4 columns">asddsa</div>
 					<div class="large-6 columns">asdasd</div>
 				</div>
+				<div class="row app-services-main" data-equalizer>
+					<div class="large-3 columns">
+						<div class="panel radius text-center" data-equalizer-watch>
+							<span class="icon-services"><i class="fi-clipboard-pencil"></i> <i class="fi-arrow-right"></i> <i class="fi-clipboard-notes"></i></span>
+							<p class="text-justify app-info-service">Crea tus reportes apartir de anotaciones y fotografías, en cada uno de los proyectos
+							en los que colaboras de una forma mucho más sencilla.</p>
+						</div>
+					</div>
+					<div class="large-4 columns">
+						<div class="panel callout radius text-center" data-equalizer-watch>
+							<span class="icon-services"><i class="fi-torso-business"></i> <i class="fi-arrow-right"></i> <i class="fi-torsos-all"></i> </span>
+							<p class="text-justify app-info-service">Publica tus proyectos y compartelos solo con tu equipo, con esto
+							tu equipo de trabajo podrá seleccionar en que proyecto participar, haciendose responsable de culminar el proyecto.</p>
+						</div>
+					</div>
+					<div class="large-3 columns">
+						<div class="panel radius" data-equalizer-watch>
+							<span class="icon-services"><i class="fi-"></i></span>
+							<p class="text-justify app-info-service">Además tendrás las siguientes opciones:</p>
+							<p class="text-justify app-info-service">
+								<ul class="app-info-service">
+									<li>Gestión de tus proyectos.</li>
+									<li>Interacción entre los equipos de trabajo.</li>
+									<li>Comunicación mediante Mensajes y Comentarios.</li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="large-9 large-centered column">
+						<p class="text-center app-span-main">La primer aplicación que mejorará tu productividad en la realización de tus reportes, 
+						sin demoras, ni contratiempos, usando la tecnología móvil como principal intermediario. </p>
+					</div>
+				</div>
 			</div>
 		</div>
 		<!-- End Main body -->
