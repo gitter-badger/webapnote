4386463bbdbfead0a7f0b1b428849153d38cc6f7
coderdiaz <javier.diaz.internaut@gmail.com>
2014-04-15 02:29:29 -0500

    Modificación de información y pixeles

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,9 @@
 		<!-- Scripts for the app -->
 		<script type="text/javascript" src="<?=base_url('assets/js/vendor/jquery.js');?>"></script>
 		<script type="text/javascript" src="<?=base_url('assets/js/foundation.min.js');?>"></script>
-		<script type="text/javascript" src="<?=base_url('assets/js/foundation/foudnation.equalizer.js');?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/app.js');?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/foundation/foundation.equalizer.js');?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/foundation/foundation.reveal.js');?>"></script>
 		<script>$(document).foundation();</script>
 	</body>
 </html>
