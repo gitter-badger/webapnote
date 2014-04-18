c5882c490e60b1760ccb2a5712d3f6862fc80225
coderdiaz <javier.diaz.internaut@gmail.com>
2014-04-18 02:16:14 -0500

    Desarrollando Dashboard

---
+++ application/views/dashboard.php
@@ -0,0 +1,37 @@
+<?php $this->load->view('header'); ?>
+	
+	<!-- Main body -->
+	<div class="app-main-body clearfix">
+		<div class="large-1 columns app-sidebar-main dash-fix-sidebar">
+			<div class="row" style="margin-bottom: 70px;">
+				<div class="large-10">
+					<a href="#" class="app-logo-dash-menu">Apnote</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="large-10">
+					<ul class="dash-menu">
+						<li>
+							<a href="#">Compañia</a>
+						</li>
+						<li>
+							<a href="#">Usuarios</a>
+						</li>
+						<li>
+							<a href="#">Proyectos</a>
+						</li>
+						<li>
+							<a href="#">Clientes</a>
+						</li>
+						<li>
+							<a href="#">Reportes</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="large-9 columns app-content-main"></div>
+	</div>
+	<!-- End Main body -->
+
+<?php $this->load->view('footer'); ?>
