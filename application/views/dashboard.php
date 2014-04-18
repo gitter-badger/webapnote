985c9774d3e9056ce994e8607d4eb449e950e222
coderdiaz <javier.diaz.internaut@gmail.com>
2014-04-18 02:35:39 -0500

    Canvas Mod for Dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,35 +2,23 @@
 	
 	<!-- Main body -->
 	<div class="app-main-body clearfix">
-		<div class="large-1 columns app-sidebar-main dash-fix-sidebar">
+		<div class="large-2 columns app-sidebar-main dash-fix-sidebar">
 			<div class="row" style="margin-bottom: 70px;">
 				<div class="large-10">
 					<a href="#" class="app-logo-dash-menu">Apnote</a>
 				</div>
 			</div>
-			<div class="row">
-				<div class="large-10">
-					<ul class="dash-menu">
-						<li>
-							<a href="#">Compañia</a>
-						</li>
-						<li>
-							<a href="#">Usuarios</a>
-						</li>
-						<li>
-							<a href="#">Proyectos</a>
-						</li>
-						<li>
-							<a href="#">Clientes</a>
-						</li>
-						<li>
-							<a href="#">Reportes</a>
-						</li>
+			<div class="large-10">
+				<aside class="left-off-canvas-menu dash-fix-canvas">
+					<ul class="off-canvas-list">
+						<li><label>Foundation</label></li>
+						<li><a href="#">One</a></li>
+						<li><a href="#">Two</a></li>
 					</ul>
-				</div>
+				</aside>
 			</div>
 		</div>
-		<div class="large-9 columns app-content-main"></div>
+		<div class="large-8 columns app-content-main"></div>
 	</div>
 	<!-- End Main body -->
 
