d5dfbe4b31b8194236d03765d2feca803dd4678b
coderdiaz <javier.diaz.internaut@gmail.com>
2014-04-24 14:30:45 -0500

    Dash Server request

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,30 +2,9 @@
 	
 	<!-- Main body -->
 	<div class="app-main-body clearfix">
-		<div class="large-2 columns app-sidebar-main">
-			<div class="row dash-app">
-				<div class="large-4 columns">
-					<img class="th" src="<?=base_url('assets/img/thumbs');?>/<?=$this->session->userdata('u_photo');?>" height="70" width="70" />
-				</div>
-				<div class="large-6 columns dash-app-profile">
-					<h6 style="color: #EEE; font-weight: 600; font-size: 14px;"><?=$this->session->userdata('u_nombre');?> <?=$this->session->userdata('u_apep');?></h6>
-					<h6 class="subheader" style="font-size: 12px;"><?=$this->session->userdata('u_username');?></h6>
-					<h6 class="subheader" style="font-size: 12px;"><span><i style="color: #59A336;" class="fi-burst"></i> Conectado</span></h6>
-				</div>
-			</div>
-			<div class="row">
-				<div class="large-10 column">
-					<ul class="side-nav">
-						<li class="active"><a href="#"><i class="fi-home icon-menu-nav"></i> Dashboard</a></li>
-						<li><a href="#"><i class="fi-comment icon-menu-nav"></i> Notificaciones <span class="badge-icon rounded right">5</span></a></li>
-						<li><a href="#"><i class="fi-database icon-menu-nav"></i> Proyectos</a></li>
-						<li><a href="#"><i class="fi-web icon-menu-nav"></i> Organizaciones</a></li>
-						<li><a href="#"><i class="fi-page-multiple icon-menu-nav"></i> Reportes</a></li>
-						<li><a href="#"><i class="fi-mail icon-menu-nav"></i> Mensajes Privados <span class="badge-icon rounded right">30</span></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
+
+		<?php $this->load->view('dash-sidebar');?>
+		
 		<div class="large-8 columns app-content-main app-content">
 			<div id="options-menu-dash-app">
 				<div class="row">
